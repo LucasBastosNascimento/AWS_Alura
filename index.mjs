08ed0c855b414a21185458369bbbe5e091f49e76
lucas bastos <lucas.bastos.nas@gmail.com>
2024-08-27 19:16:06 -0300

    Commit inicial

---
+++ index.mjs
@@ -0,0 +1,8 @@
+import { log } from 'log';
+export const handler = async (event) => {
+  log('Log de execução' + JSON.stringify(event)); 
+  return {
+    statusCode: 200,
+    body: JSON.stringify(event),
+  };
+};
