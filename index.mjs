ee0167a6fae2a7880f655ea954ed35ba01328f19
lucas bastos <lucas.bastos.nas@gmail.com>
2024-08-29 18:42:52 -0300

    Teste inicio

--- index.mjs
+++ index.mjs
@@ -1,6 +1,11 @@
 import { log } from 'log';
+import { S3 } from '@aws-sdk/client-s3'
 export const handler = async (event) => {
-  log('Log de execução' + JSON.stringify(event)); 
+ 
+    const record = event.Records[0];
+    const Bucket = record.s3.bucket.name;
+    const Key = record.s3.object.key;
+
   return {
     statusCode: 200,
     body: JSON.stringify(event),
